b3d087b0de9090a659a5e4353a57a993a3785b01
Varshini Byri <145209852+ByriVarshini@users.noreply.github.com>
2025-04-18 13:12:48 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -38,3 +38,9 @@ def serve_homepage():
     return FileResponse(INDEX_PATH)
 
 
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+
+
