41f16f9c66331d1b1ea6e555f121dd36cb6c0390
Varshini Byri <145209852+ByriVarshini@users.noreply.github.com>
2025-07-28 20:02:38 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,32 +1,19 @@
-# import psycopg2
-# from psycopg2.extras import RealDictCursor
-
-# def get_db():
-#     conn = psycopg2.connect(
-#         host="localhost",
-#         database="fastapi",
-#         user="postgres",
-#         password="varshu2212",
-#         cursor_factory=RealDictCursor
-#     )
-#     try:
-#         cursor = conn.cursor()
-#         yield cursor 
-#     finally:
-#         cursor.close()
-#         conn.close()
-
-from psycopg2 import connect
+import psycopg2
 from psycopg2.extras import RealDictCursor
 
 def get_db():
-    conn = connect(
-        "postgresql://postgres.hbmjmbgbuxhygzrdkeuj:funblog@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres",
+    conn = psycopg2.connect(
+        host="localhost",
+        database="fastapi",
+        user="postgres",
+        password="varshu2212",
         cursor_factory=RealDictCursor
     )
     try:
         cursor = conn.cursor()
-        yield cursor
+        yield cursor 
     finally:
         cursor.close()
         conn.close()
+
+
